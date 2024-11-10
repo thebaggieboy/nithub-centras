8b0ff1400af58ec72a01c2af70e99684d5f2c264
dot-enny <olaniyaneniola0225@gmail.com>
2024-11-10 21:41:27 +0100

    fix: ui responsiveness.

--- components/landing-page/sections/Features.tsx
+++ components/landing-page/sections/Features.tsx
@@ -3,20 +3,20 @@ import Image from "next/image"
 export const FeatureSection = () => {
     return (
       <div className="min-h-screen bg-[#020313] flex justify-center items-center">
-        <div className="lg:w-[94%] xl:w-[86%] flex gap-y-10 justify-start max-lg:flex-col lg:justify-between xl:grid xl:grid-cols-2 items-center">
+        <div className="max-sm:max-w-[min(90%,_300px)] mx-auto lg:w-[94%] xl:w-[86%] flex gap-y-10 justify-start max-lg:flex-col lg:justify-between xl:grid xl:grid-cols-2 items-center max-w-screen-2xl max-sm:px-5">
   
-          <Image src="/img/growth-illustration.svg" alt="illustration of a graph" width={360} height={304.96} className="" />
+          <Image src="/img/growth-illustration.svg" alt="illustration of a graph" width={360} height={304.96} />
   
           <div className="grid gap-10">
-            <h2 className="text-4xl lg:text-5xl font-medium flex flex-col leading-extra-snug">
+            <h2 className="text-3xl sm:text-4xl lg:text-5xl font-medium flex flex-col leading-extra-snug">
               <span className="text-[#98A1B3]">Stay Organized & </span>
               <span className="text-white">Grow Your Business</span>
             </h2>
             <div className="flex flex-wrap gap-3 lg:w-max">
-              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3">Vendors</span>
-              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3">Customers</span>
-              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3">Administrators</span>
-              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3">Business Owners</span>
+              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3 max-sm:order-3">Vendors</span>
+              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3 max-sm:order-1">Customers</span>
+              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3 max-sm:order-2">Administrators</span>
+              <span className="text-[#98A1B3] border border-[#98A1B3] p-1 rounded-full py-1 px-3 max-sm:order-4">Business Owners</span>
             </div>
           </div>
         </div>
--- components/landing-page/sections/Hero.tsx
+++ components/landing-page/sections/Hero.tsx
@@ -2,24 +2,26 @@ import Image from "next/image";
 
 export const HeroSection = () => {
     return (
-        <div className="bg-[url('/img/hero-bg.svg')] bg-no-repeat bg-cover h-screen flex justify-center items-center relative">
-            <Navbar />
-            <div className="lg:w-[94%] xl:w-[86%] grid lg:grid-cols-2 mt-10">
-                <div className="max-w-[600px] mx-auto">
-                    <h2 className="font-bold text-4xl xl:text-5xl leading-extra-snug xl:leading-extra-snug">
-                        Empower Your Business with
-                        <span className="text-primary"> Seamless CRM</span>
-                        <span> & </span>
-                        <span className="text-success">Inventory Solutions.</span>
-                    </h2>
-                    <p className="leading-extra-snug mt-2 mb-8">Centra optimizes your customer relationships and inventory management, helping you save time, reduce costs, and grow your business effortlessly.</p>
-                    <button className="bg-primary text-white px-5 py-3 rounded-lg">Get Started</button>
-                </div>
+        <div className="bg-[url('/img/hero-bg.svg')] bg-no-repeat bg-cover">
+            <div className="max-w-screen-2xl mx-auto flex justify-center items-center h-screen relative max-sm:px-5">
+                <Navbar />
+                <div className="lg:w-[94%] xl:w-[86%] grid lg:grid-cols-2 mt-10">
+                    <div className="max-w-[600px] mx-auto">
+                        <h2 className="font-bold text-4xl xl:text-5xl leading-extra-snug xl:leading-extra-snug">
+                            Empower Your Business with
+                            <span className="text-primary"> Seamless CRM</span>
+                            <span> & </span>
+                            <span className="text-success">Inventory Solutions.</span>
+                        </h2>
+                        <p className="leading-extra-snug mt-2 mb-8">Centra optimizes your customer relationships and inventory management, helping you save time, reduce costs, and grow your business effortlessly.</p>
+                        <button className="bg-primary text-white px-5 py-3 rounded-lg">Get Started</button>
+                    </div>
 
-                <div className="max-lg:hidden relative">
-                    <Image src="/img/table.svg" alt="illustration of a table" width={1000} height={278.86} className="absolute -left-10 -top-[120px] xl:-top-[150px]" />
-                    <Image src="/img/graph.svg" alt="illustration of a graph" width={330} height={278.88} className="absolute left-[244px] top-[74px] [box-shadow:_3.6px_3.6px_90px_36px_#E3EDFD80;] rounded-[5.64px] max-xl:hidden" />
-                    <Image src="/img/graph.svg" alt="illustration of a graph" width={260} height={278.88} className="absolute left-[180px] top-[52px] [box-shadow:_3.6px_3.6px_90px_36px_#E3EDFD80;] rounded-[5.64px] xl:hidden" />
+                    <div className="max-lg:hidden relative">
+                        <Image src="/img/table.svg" alt="illustration of a table" width={1000} height={278.86} className="absolute -left-10 -top-[120px] xl:-top-[150px]" />
+                        <Image src="/img/graph.svg" alt="illustration of a graph" width={330} height={278.88} className="absolute left-[244px] top-[74px] [box-shadow:_3.6px_3.6px_90px_36px_#E3EDFD80;] rounded-[5.64px] max-xl:hidden" />
+                        <Image src="/img/graph.svg" alt="illustration of a graph" width={260} height={278.88} className="absolute left-[180px] top-[52px] [box-shadow:_3.6px_3.6px_90px_36px_#E3EDFD80;] rounded-[5.64px] xl:hidden" />
+                    </div>
                 </div>
             </div>
         </div>
